0752f27a7234979a7e69c0a225401623385d0049
cpasion-office-win10 <373704015@qq.com>
2022-04-26 08:57:35 +0800

    fix(shell.py): 修复结束命令运行成功但却返回False

--- core/shell.py
+++ core/shell.py
@@ -107,8 +107,8 @@ def run_command(
 
         if stderr:
             # raise Exception('run_command() 结果出错:', stderr)
-            print('run_command() 结果出错:', stderr)
-            return { "err":stderr, "success":False }
+            print('run_command() 命令完成:', stderr)
+            return { "res":stderr, "success":True }
 
         return { "success":False, "err":"nothing change" }
 
